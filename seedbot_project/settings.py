696f6119c9fa57bff4cb290fac6571ce265685c5
Will Jones <wrjones@gmail.com>
2025-08-14 11:59:49 +0000

    Production changes to fix bugs with deployment

--- seedbot_project/settings.py
+++ seedbot_project/settings.py
@@ -4,45 +4,31 @@ from dotenv import load_dotenv
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Load the .env file from the bot's directory
+# Load the .env file specified in your gunicorn.service file
+# Note: Ensure the .env file path is correct relative to your bot's location.
 ENV_PATH = BASE_DIR.parent / 'seedbot2000' / '.env'
 load_dotenv(dotenv_path=ENV_PATH)
 
-# --- SECRETS ---
-# All secrets are loaded from your .env file
+# --- SECRETS (Loaded from .env file) ---
 SECRET_KEY = os.getenv('SECRET_KEY')
 WC_API_KEY = os.getenv('new_api_key')
 BOT_TOKEN = os.getenv('your_bot_token_env_name')
 
-# --- PRODUCTION vs DEVELOPMENT ---
-# We use an environment variable to decide which mode we're in.
-# On your server, we will set this to 'production'.
-if os.getenv('DJANGO_ENV') == 'production':
-    DEBUG = False
-    ALLOWED_HOSTS = ['seedbot.net', 'www.seedbot.net']
-    CSRF_TRUSTED_ORIGINS = ['https://seedbot.net', 'https://www.seedbot.net']
-    CSRF_COOKIE_SECURE = True
-    SESSION_COOKIE_SECURE = True
-    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-else:
-    # Default settings are for local development
-    DEBUG = True
-    ALLOWED_HOSTS = []
+# --- PRODUCTION SETTINGS (Hardcoded) ---
+DEBUG = False
+ALLOWED_HOSTS = ['seedbot.net', 'www.seedbot.net']
+CSRF_TRUSTED_ORIGINS = ['https://seedbot.net', 'https://www.seedbot.net']
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-# --- INSTALLED APPS & MIDDLEWARE (Consolidated) ---
+# --- Application Definition ---
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'django.contrib.sites',
-    'django.contrib.redirects',
+    'django.contrib.admin', 'django.contrib.auth', 'django.contrib.contenttypes',
+    'django.contrib.sessions', 'django.contrib.messages', 'django.contrib.staticfiles',
+    'django.contrib.sites', 'django.contrib.redirects',
     'presets',
-    'allauth',
-    'allauth.account',
-    'allauth.socialaccount',
+    'allauth', 'allauth.account', 'allauth.socialaccount',
     'allauth.socialaccount.providers.discord',
 ]
 
@@ -60,18 +46,10 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'seedbot_project.urls'
 
-AUTHENTICATION_BACKENDS = [
-    # Needed to login by username in Django admin, regardless of `allauth`
-    'django.contrib.auth.backends.ModelBackend',
-
-    # `allauth` specific authentication methods, such as login by email
-    'allauth.account.auth_backends.AuthenticationBackend',
-]
-
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'], # Add this line
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -86,81 +64,37 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'seedbot_project.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
-import os
-
-import os
-from pathlib import Path
-
-BASE_DIR = Path(__file__).resolve().parent.parent
-
+# --- Database Configuration ---
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    },
+    'default': {'ENGINE': 'django.db.backends.sqlite3', 'NAME': BASE_DIR / 'db.sqlite3'},
     'seedbot_db': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR.parent / 'seedbot2000' / 'db' / 'seeDBot.sqlite',
     }
 }
-
-
-# Password validation
-# https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
-
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
-
-
-# Internationalization
-# https://docs.djangoproject.com/en/5.2/topics/i18n/
-
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
-USE_I18N = True
-
-USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.2/howto/static-files/
-
-STATIC_URL = 'static/'
-
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-SITE_ID = 1
-LOGIN_REDIRECT_URL = '/'
-
 DATABASE_ROUTERS = ['seedbot_project.db_router.SeedBotRouter']
 
-CSRF_COOKIE_SECURE = True
-SESSION_COOKIE_SECURE = True
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+# --- Password validation, Internationalization, etc. ---
+AUTH_PASSWORD_VALIDATORS=[{'NAME':'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},{'NAME':'django.contrib.auth.password_validation.MinimumLengthValidator'},{'NAME':'django.contrib.auth.password_validation.CommonPasswordValidator'},{'NAME':'django.contrib.auth.password_validation.NumericPasswordValidator'}]
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'UTC'
+USE_I18N = True
+USE_TZ = True
+
+# --- Static files ---
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# --- Django-Allauth & Sites Framework ---
+AUTHENTICATION_BACKENDS = ['django.contrib.auth.backends.ModelBackend', 'allauth.account.auth_backends.AuthenticationBackend']
+SITE_ID = 1
+LOGIN_REDIRECT_URL = '/'
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SOCIALACCOUNT_PROVIDERS = {
+    'discord': {
+        'SCOPE': ['identify', 'email'],
+        'AUTH_PARAMS': {'access_type': 'online'},
+    }
+}
